f80089359f2af17d159d24afaa5101c8bf3da3d5
dayoon97 <ekdbsekdbs@gmail.com>
2021-02-25 16:07:38 +0900

    폰트 변경, 글자수 제한 변경

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../resources/CSS/Content.css';
 import redux from '../resources/images/redux.png';
 import jake from '../resources/images/jake.jpg';
 
 const Content = () => {
+    const [board, setBoard] = useState([]);
+    useEffect(() => {
+        fetch('/api/board')
+        .then(response => response.json())
+        .then(data => setBoard(data));
+    }, []);
+    
     return (
+    <React.Fragment>
+       {board.map(b => 
         <div className="content">
             <div className="con"><img src={jake} alt="jake" width="100%" height="100%" /></div>
             <div className="profile"><img src={jake} alt="jake" width="100%" height="100%"/></div>
-            <div className="id">dayoon97</div>
-            <div className="title">벨로그 컴포넌트</div>
-            <div className="date">2021년 2월 22일 금요일. 2개의 댓글</div>
-            <div className="detail">내용 첫줄</div>
+            <div className="id">{b.boardWriter}</div>
+            <div className="title">{b.boardName.length > 20 ? b.boardName.substr(0, 20) + '.  .  . ' : b.boardName}</div>
+            <div className="date">{b.boardRegDate}</div>
+            <div className="detail">{b.boardContent.length > 20 ? b.boardContent.substr(0, 20) + '. . .' : b.boardContent}</div>
         </div>
+       )}
+    </React.Fragment>            
+        
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Content from '../components/Content';
 import '../resources/CSS/home.css';
 
 
 const Home = () => {
-    const [message, setMessage] = useState("");
-        useEffect(() => {
-            fetch('/api/board')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .then(message => {
-            setMessage(message);
-            console.log(message);
-        });
-    },[])
-
-
     return (
         <div className="dashboard">
-                <p id="post">지금 뜨는 포스트 {message}</p>
-                <Content/>
-                <Content/>
-                <Content/>
-                <Content/>
-                <Content/>
+                <p id="post">지금 뜨는 포스트 {}</p>
                 <Content/>
         </div>
     );
