e65fa70e1469607be494d38f77ed8ef638e9fe84
dayoon97 <ekdbsekdbs@gmail.com>
2021-02-25 15:09:26 +0900

    스프링 부트 연결
    데이터 가져오기 완료

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Content from '../components/Content';
 import '../resources/CSS/home.css';
 
 
 const Home = () => {
+    const [message, setMessage] = useState("");
+        useEffect(() => {
+            fetch('/api/board')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then(message => {
+            setMessage(message);
+            console.log(message);
+        });
+    },[])
+
+
     return (
         <div className="dashboard">
-                <p id="post">지금 뜨는 포스트</p>
+                <p id="post">지금 뜨는 포스트 {message}</p>
                 <Content/>
                 <Content/>
                 <Content/>
